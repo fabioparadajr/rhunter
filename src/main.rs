9aefb8a52399d7d8e2c681cb1b07317be445e2e7
fabio <fabio.198@hotmail.com>
2025-03-07 14:12:55 -0300

    Adding function scriber into main

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,14 @@
 
 use fanotify::{low_level::*,  high_level::*};
 use hasher::get_file_hash;
-use std::{fmt::LowerHex, fs::metadata, path::{Path, PathBuf}, thread};
+use logger::scriber;
+use std::path::PathBuf;
 
 mod hasher;
-
+mod logger;
 fn main() {
-/* ###### PERMISSÃO ##### para o binario ter privilegio similiar de root: sudo setcap cap_sys_admin=eip rhunter
+/* ###### PERMISSION ##### binary to have similar priviliges as root: sudo setcap cap_sys_admin=eip rhunter
 
-###### Leitura dos eventos ################
-CloseNowrite -> abriu e não escreveu( posso ignorar)
-Quando abre o arquivo cria arquivo.txt.swp, e nele aparece modify e depois deleted.        
-    
  */ 
     let primary_path = PathBuf::from("/home/fabio/teste_av");
     let ft = Fanotify::new_nonblocking(FanotifyMode::NOTIF).expect("Error regitering fanotify listener");
@@ -20,18 +17,20 @@ Quando abre o arquivo cria arquivo.txt.swp, e nele aparece modify e depois delet
         &primary_path,
     ).unwrap();
 
-    let mut diretorios:Vec<PathBuf> = vec![primary_path];
+    let mut directories:Vec<PathBuf> = vec![primary_path];
     
 
     loop {
         let eventos = ft.read_event();
         for events in eventos {
+            
             let write = FanEvent::CloseWrite;    
             let file_path = PathBuf::from(&events.path);
+            scriber(&events.events, &file_path);
             if events.events.contains(&write) && !events.path.contains("swp") && file_path.is_file()
             {
 
-                println!("Arquivo alterado: {:?}", events.path );
+                println!("File changed: {:?}", events.path );
                
                    let hsh =  match get_file_hash(&file_path)
                    {
@@ -40,20 +39,22 @@ Quando abre o arquivo cria arquivo.txt.swp, e nele aparece modify e depois delet
                     };
                 
 
-            } else if file_path.is_dir() && !diretorios.contains(&file_path){ // Need to be a dir and not inside the vector.
-                    
+            } else if file_path.is_dir() && !directories.contains(&file_path){ // Need to be a dir and not inside the vector.
+                /* ################### add new dir #######
+              At first, I thought of this as recursive, when the user provides a path and i need to look everything inside and them start monitor
+                but I don't need that. 
+              Since the events dynamically show any changes in the directories, they will automatically be added to the vector and start monitoring."
+                 */  
                 println!("Novo diretorio adicionado ao monitoramento: {:?}", file_path);
                 let new_path = PathBuf::from(&events.path);
-                diretorios.push(new_path.clone());
+                directories.push(new_path.clone());
                 ft.add_path(
                     FAN_ACCESS | FAN_CLOSE | FAN_EVENT_ON_CHILD | FAN_MODIFY | FAN_ONDIR,
                     &file_path,
                 ).unwrap();
                 
                 
-            } else {
-                println!("ignorado: {:?}", events)
-            }
+            } 
             
         }
 
