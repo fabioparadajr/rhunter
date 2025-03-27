f67e8bfa722b6d9cfdf0d33e75d6d945fbe43230
fabio <fabio.198@hotmail.com>
2025-03-27 15:16:28 -0300

    Formatting right

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,36 @@
 use monitor::monitor_dir;
-use regex::Regex;
 use std::{path::PathBuf, sync::Arc, time::Instant};
+use regex::Regex;
 
+mod monitor;
 mod hasher;
 mod logger;
-mod monitor;
 fn main() {
-    /* ###### PERMISSION ##### binary to have similar priviliges as root: sudo setcap cap_sys_admin=eip rhunter
 
-    */
 
-    let rgx: Regex = Regex::new(r"^(/([a-z0-9_-]+/)*[a-z0-9_-]+)?/$").unwrap();
-    let path;
-    loop {
-        println!("Hello, type a path to monitor: ");
-        let mut input = String::new();
-        std::io::stdin().read_line(&mut input).unwrap();
-        let input_trim = input.trim();
+let rgx: Regex = Regex::new(r"^(/([a-z0-9_-]+/)*[a-z0-9_-]+)?/$").unwrap();
+let path ;
+loop {
+    println!("Hello, type a path to monitor: ");
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+    let input_trim = input.trim();
 
-        if rgx.is_match(&input_trim) {
-            path = String::from(input_trim);
-            println!("Path: {}", path);
+    if rgx.is_match(&input_trim) {
+        path = String::from(input_trim);
+        println!("Path: {}", path);
 
-            break;
-        } else {
-            println!("Ops! You didn't type right.");
-        }
+        break;
+    } else {
+        println!("Ops! You didn't type right.");
     }
+}
+
 
     monitor_dir(path);
-}
+
+
+
+    
+    
+    }
