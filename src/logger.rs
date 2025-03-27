21d534a353d30dda5f452b8fb6e450aa1188edd0
fabio <fabio.198@hotmail.com>
2025-03-27 15:08:40 -0300

    Formatting

--- src/logger.rs
+++ src/logger.rs
@@ -4,7 +4,6 @@ use std::fs::OpenOptions;
 use std::io::Write;
 use std::path::Path;
 
-
 pub fn scriber(event: &Vec<FanEvent>, event_path: &Path) -> Result<(), std::io::Error> {
     let local: DateTime<Local> = Local::now();
     let mut file = OpenOptions::new()
--- src/monitor.rs
+++ src/monitor.rs
@@ -1,54 +1,46 @@
-
-use fanotify::{low_level::*,  high_level::*};
-use std::{path::PathBuf, sync::Arc};
-use crate::logger;
 use crate::hasher;
+use crate::logger;
+use fanotify::{high_level::*, low_level::*};
+use std::{path::PathBuf, sync::Arc};
 
 pub fn monitor_dir(path: String) -> String {
+    let primary_path = PathBuf::from(path);
+    let ft =
+        Fanotify::new_nonblocking(FanotifyMode::NOTIF).expect("Error regitering fanotify listener");
+    /*let _real_path = ft.add_path(
+        FAN_ACCESS | FAN_CLOSE | FAN_EVENT_ON_CHILD | FAN_MODIFY | FAN_ONDIR | FAN_CREATE,
+        &primary_path,
+    ).unwrap();
+     */
 
-let primary_path = PathBuf::from(path);
-let ft = Fanotify::new_nonblocking(FanotifyMode::NOTIF).expect("Error regitering fanotify listener");
-/*let _real_path = ft.add_path(
-    FAN_ACCESS | FAN_CLOSE | FAN_EVENT_ON_CHILD | FAN_MODIFY | FAN_ONDIR | FAN_CREATE,
-    &primary_path,
-).unwrap();
- */
+    let real_path = ft.add_mountpoint(
+        FAN_ACCESS | FAN_CLOSE | FAN_EVENT_ON_CHILD | FAN_MODIFY | FAN_ONDIR,
+        &primary_path,
+    );
+    //let mut directories:Vec<PathBuf> = vec![primary_path];
 
- let real_path = ft.add_mountpoint(FAN_ACCESS | FAN_CLOSE | FAN_EVENT_ON_CHILD | FAN_MODIFY | FAN_ONDIR, &primary_path);
-//let mut directories:Vec<PathBuf> = vec![primary_path];
+    let path = primary_path.to_str().unwrap();
 
+    loop {
+        let eventos = ft.read_event();
+        for events in eventos {
+            let write = FanEvent::CloseWrite;
+            let file_path = Arc::new(PathBuf::from(&events.path));
 
+            if events.path.contains(path) {
+                logger::scriber(&events.events, &file_path);
+                if events.events.contains(&write)
+                    && !events.path.contains("swp")
+                    && file_path.is_file()
+                {
+                    println!("File changed: {:?}", events.path);
 
-let path = primary_path.to_str().unwrap();
-
-loop {
-    let eventos = ft.read_event();
-    for events in eventos {
-        
-        let write = FanEvent::CloseWrite;    
-        let file_path = Arc::new(PathBuf::from(&events.path));
-        
-            
-            
-        if events.path.contains(path){
-            logger::scriber(&events.events, &file_path);
-            if events.events.contains(&write) && !events.path.contains("swp") && file_path.is_file()
-            {
-                
-                println!("File changed: {:?}", events.path );
-               
-                   let hsh =  match hasher::get_file_hash(Arc::clone(&file_path))
-                   {
+                    let hsh = match hasher::get_file_hash(Arc::clone(&file_path)) {
                         Ok(hex) => println!("hash: {hex}"),
-                        Err(e) => println!("Couldnt hash the file")
+                        Err(e) => println!("Couldnt hash the file"),
                     };
-
+                }
             }
-            
-
         }
-        
     }
-
-}
 }
